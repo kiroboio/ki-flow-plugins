46800a4d7e723c0bb46bc8e89c1ee331675ff7d1
Brenzee <karelisbrendons@gmail.com>
2023-09-07 13:24:09 +0300

    Curve - initial new simple plugins

---
+++ src/plugins/Curve/constants.ts
@@ -0,0 +1,12 @@
+import { SupportedContract } from "../../types";
+
+export const UniswapV2_Router: readonly SupportedContract[] = [
+  {
+    address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    chainId: "1",
+  },
+  {
+    address: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
+    chainId: "5",
+  },
+] as const;
---
+++ src/plugins/Curve/plugins.ts
@@ -0,0 +1,11 @@
+import { createProtocolPluginsAsObject } from "../../Plugin";
+import { CurvePoolABI } from "./ABI";
+
+// For Curve there should be 3 different plugins, because pool has strict array lengths and pool can have 2, 3 or 4 assets.
+// For example, uint256[2], uint256[3], uint256[4] etc.
+
+const CurvePool = createProtocolPluginsAsObject({
+  abi: CurvePoolABI,
+});
+
+export const Curve = { ...CurvePool };
