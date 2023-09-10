97dadfe2a9a65cc62093b610b3b4fdf517386c75
Brenzee <karelisbrendons@gmail.com>
2023-09-10 12:36:39 +0300

    Multicall - comments

--- src/plugins/Multicall/plugins.ts
+++ src/plugins/Multicall/plugins.ts
@@ -1,4 +1,6 @@
 import { createProtocolPluginsAsObject } from "../../Plugin";
 import { KiroboMulticallABI } from "./ABI";
 
+// TODO: Multicall getter plugins should be smart plugins
+// In that way we can calculate correct outputs for multicall
 export const Multicall = createProtocolPluginsAsObject({ abi: KiroboMulticallABI });
