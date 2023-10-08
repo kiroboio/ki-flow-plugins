fd19981f1c533a3c7acfc92d80c19862bff22cc0
Brenzee <karelisbrendons@gmail.com>
2023-10-08 12:24:01 +0300

    update - NonfungiblePositionManager Simple Plugins

--- src/plugins/UniswapV3/plugins.ts
+++ src/plugins/UniswapV3/plugins.ts
@@ -3,14 +3,19 @@ import { ProtocolPlugins } from "../../types";
 import { UniswapV3_Router_ABI } from "./ABI";
 import { UniswapV3_Router } from "./constants";
 
-// TODO: Add NonfungiblePositionManager plugins - mint, increaseLiquidity, decreaseLiquidity, collect
 const Router = createProtocolPluginsAsObject({
-  protocol: "UniswapV3",
+  protocol: "UniswapV3_Router",
   abi: UniswapV3_Router_ABI,
   supportedContracts: UniswapV3_Router,
 });
 
-export const UniswapV3 = { ...Router };
+const NonfungiblePositionManager = createProtocolPluginsAsObject({
+  protocol: "UniswapV3_NonfungiblePositionManager",
+  abi: UniswapV3_Router_ABI,
+  supportedContracts: UniswapV3_Router,
+});
+
+export const UniswapV3 = { ...Router, ...NonfungiblePositionManager };
 
 export type IUniswapV3 = typeof UniswapV3;
 export type UniswapV3Array = ProtocolPlugins<IUniswapV3>;
