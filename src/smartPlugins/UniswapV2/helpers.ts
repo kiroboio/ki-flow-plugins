a9a64296e7c1a77939f3d75fea81083cd957f907
Brenzee <karelisbrendons@gmail.com>
2023-10-08 12:13:39 +0300

    info - remove unused code

--- src/smartPlugins/UniswapV2/helpers.ts
+++ src/smartPlugins/UniswapV2/helpers.ts
@@ -12,26 +12,6 @@ export function createToken(token: { address: string; decimals: number }, chainI
   return new Token(chainId, token.address, +token.decimals);
 }
 
-// This is to remove unnecessary properties from the output type. Use it eg. `ExtractPropsFromArray<Inventory.ItemStructOutput>`
-export type ExtractPropsFromArray<T> = Omit<T, keyof Array<unknown> | `${number}`>;
-
-// convertToStruct takes an array type eg. Inventory.ItemStructOutput and converts it to an object type.
-export const handleInput = <A extends Array<unknown>>(arr: A): ExtractPropsFromArray<A> => {
-  const keys = Object.keys(arr).filter((key) => isNaN(Number(key)));
-  const result = {};
-  // @ts-ignore
-  arr.forEach((item, index) => (result[keys[index]] = handleValue(item)));
-  return result as A;
-};
-
-// @ts-ignore
-export const handleValue = (value: any): any => {
-  if (typeof value === "string" || typeof value === "boolean") return value;
-  if (typeof value === "number") return value.toString();
-  if (value instanceof ethers.BigNumber) return value.toString();
-  if (value instanceof Array) return value.map((v) => handleValue(v));
-};
-
 export const getPluginFromRoute = ({
   chainId,
   route,
